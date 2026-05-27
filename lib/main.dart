e903e40b955db719da394a3ba56ddbfe17a23d9b
Vitor Emanuel <vitor.emanuelsilva2004@gmail.com>
2026-05-26 23:08:51 -0300

    Reformulado codigo para melhorar conexao com firebase database e autenticacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/app_theme.dart';
+import 'firebase_options.dart';
 import 'pages/splash/splash_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const RuSmartApp());
 }
 
